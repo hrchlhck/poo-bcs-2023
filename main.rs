640f63dbec814b41b9c72a9353ee49e599fd1a7d
hrchlhck <vpemfh7@protonmail.com>
2023-05-24 22:05:12 -0300

    Piorando código

--- main.rs
+++ main.rs
@@ -2,6 +2,7 @@ fn main() {
     let count_of_peoples_in_the_word = 8_000_000_000;
     for _ in 0..count_of_peoples_in_the_word {
         println!("Hello, world!");
+        println!("Ola, mundo!");
     }
 }
 batata
